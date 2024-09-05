724a3e2b80e78ff34b78b343228cdbff944361c1
Tshepo1000 <tshepotubatsi@gmail.com>
2024-09-05 05:47:15 -0700

    desktop and mobile view complete

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -2,13 +2,17 @@ function openNav(){
     document.getElementById("myNav").style.width = "100%";
     document.getElementById("closebtn").style.display = "block";
     document.getElementById("hamburgerIcon").style.display = "none";
+    document.body.classList.add("no-scroll");  // Add no-scroll class
 }
 
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
     document.getElementById("closebtn").style.display = "none";
     document.getElementById("hamburgerIcon").style.display = "block";
+    document.body.classList.remove("no-scroll");  // Remove no-scroll class
 }
+
+// scroll to sections
 
 // scrolls to about section on click
 function scrollToAboutUsSection(){
@@ -23,3 +27,4 @@ function scrollToContactUsSection(){
         behavior: 'smooth'
     });
 }
+
